55662083e9c5093daacbceed014f49a7b19bfb46
KaykyFEI <feiskpinheiro@fei.edu.br>
2026-04-20 09:11:39 -0300

    Restruturação da lógica do módulo autenticacao. Ainda falta estruturar as funções de validação e a de persistência dos dados no arquivo JSON. ---------------------------------------------------------------------- Estou evitando usar bibliotecas para ter o máximo de controle sobre o algoritmo e enteder o código.

--- modulos/autenticacao.py
+++ modulos/autenticacao.py
@@ -1,10 +1,32 @@
-def cadastro(usuario, email, senha, dica):
-    novo_usuario = {
-        "usuario": usuario,
-        "email": email,
-        "senha": senha,
-        "dica": dica
-    }
+# Esse módulo contém as funções de cadastro, login e redefinação de senha
 
-    print(f"Usuário {usuario} cadastrado com sucesso!")
-    return novo_usuario
+import os
+import json
+
+arquivo = ".\dados\usuarios.json"
+
+def validarEmail(email):
+    
+
+def carregarUsuarios():
+    if not os.path.exists(arquivo):
+        return []
+    with open(arquivo, "r", encoding="utf-8") as arq:
+        try:
+            return json.load(arq)
+        except json.JSONDecodeError:
+            return[]
+
+
+def verificarDuplicata(valor, campo):
+    usuarios = carregarUsuarios()
+    for usuario in usuarios:
+        if usuario.get(campo) == valor:
+            return True
+    return False
+
+def salvarUsuario(novoUsuario):
+    usuarios = carregarUsuarios()
+    usuarios.append(novoUsuario)
+    with open(arquivo, "w", encoding="utf-8") as arq:
+        json.dump(usuarios, arq, indent=4, ensure_ascii=False)
